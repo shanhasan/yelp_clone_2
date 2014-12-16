9e7538e55e38fac7d5e9c84637189a2958ba0b6f
Shan Hasan <shan119@hotmail.co.uk>
2014-12-16 17:49:03 +0000

    added reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :restaurants do
+    resources :reviews
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,6 +58,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  resources :restaurants
+  # resources :restaurants
   
 end
