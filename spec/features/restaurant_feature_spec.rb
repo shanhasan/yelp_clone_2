3eadd1c4a35726a064a2c54a0e3ddc703365a919
Alan Bridger <alanrbridger@gmail.com>
2014-12-16 16:45:35 +0000

    Passes next test, allows user to update restaurants. Adds basic CSS

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -42,7 +42,22 @@ feature 'restaurants' do
       visit '/restaurants'
       click_link 'KFC'
       expect(page).to have_content 'KFC'
-      expect(current_path).to eq "restaurants/#{@kfc.id}"
+      expect(current_path).to eq "/restaurants/#{@kfc.id}"
+    end
+  end
+
+  context 'updating restaurants' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    it 'lets a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(current_path).to eq '/restaurants'
     end
   end
 
