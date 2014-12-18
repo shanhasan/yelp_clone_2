f70713fda23045820834fe4efaab7f790cac3e50
Alan Bridger <alanrbridger@gmail.com>
2014-12-18 12:11:00 +0000

    Average_rating method updated

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -18,7 +18,7 @@ end
 
   def leave_review(thoughts, rating)
     visit '/restaurants'
-    click_link "review KFC"
+    click_link "Review KFC"
     fill_in "Thoughts", with: thoughts
     select rating, from: 'Rating'
     click_button 'Leave Review'
